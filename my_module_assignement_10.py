5c630d499d03861f3a1fc66b75f28fa92e17895d
Filip Holmberg <filip.holmberg.se@gmail.com>
2024-09-02 15:17:39 +0200

    first commit

---
+++ my_module_assignement_10.py
@@ -0,0 +1,13 @@
+#Custom module for assignement 10.3
+
+"""This module contains 2 functions. One function to calculate addition and one to calculate multiplication"""
+
+def additions_of_numbers(a,b):
+    print("This is addition")
+    return a+b
+
+def multiplication_of_numbers(a,b):
+    print("This is multiplication")
+    return a*b
+
+print("test 2")
